dd8b20a7d2541e2a95ae90fda33a99f3a6ceeb3f
Rosine22 <116634586+Rosine22@users.noreply.github.com>
2024-11-22 10:06:22 +0000

    indexOf string

---
+++ indexOf.js
@@ -0,0 +1,11 @@
+/* Write a function indexOfIgnoreCase taking two strings and determining the first occurrence of the second string in the first string. The function should be case insensitive.
+
+Example: indexOfIgnoreCase('bit','it') and indexOfIgnoreCase('bit','IT') should return 1*/
+
+function indexOfIgnoreCase(s1, s2) {
+    let s1Lower = s1.toLowerCase();
+    let s2Lower = s2.toLowerCase();
+    return s1Lower.indexOf(s2Lower);
+  }
+  console.log((indexOfIgnoreCase('bit','IT')));
+  
